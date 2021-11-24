25cb1f761b2dcdbf64445f6d0dec1d81e8ed1ef6
jmoraal <49272820+jmoraal@users.noreply.github.com>
2021-11-24 09:52:03 +0100

    Begin implementing sticky collisions
    Colouring does not yet work properly (particles that collide at some point are gray from the beginning) and although particles have no effect on each other anymore, their coordinates are not yet identical after collision (so not quite sticky yet).

--- 2DDislocationsSDE.py
+++ 2DDislocationsSDE.py
@@ -13,26 +13,27 @@ import numba
 from numba import jit #just-in-time compiling. Yet to be implemented...
 
 
-boxLength = 7
+
+# Example 1:
+boxLength = 1
+initialPositions = np.array([[0.02,0.02], [0.2,0.8], [0.8,0.5], [0.85,0.3], [1,0.8]]) 
+charges = np.array([-1,1,1,-1,-1])
+nrParticles = np.shape(initialPositions)[0]
+dim = np.shape(initialPositions)[1]
+
+# Example 2:
+# boxLength = 3
+# nrParticles = 20
+# dim = 2
+# initialPositions = np.random.uniform(size = (nrParticles, dim), low = 0, high = boxLength)
+# charges = np.random.choice((-1,1),nrParticles)
+# charges = np.ones(nrParticles)
+
 domain = ((0,boxLength),(0,boxLength))
 #Define 0 to 1 as positive direction. 
 
-# Example 1:
-# initialPositions = np.array([[0.02,0.02], [0.2,0.8], [0.8,0.5], [0.85,0.3], [1,0.8]]) 
-# charges = np.array([-1,1,1,-1,-1])
-# nrParticles = np.shape(initialPositions)[0]
-# dim = np.shape(initialPositions)[1]
 
-
-# Example 2:
-nrParticles = 20
-dim = 2
-initialPositions = np.random.uniform(size = (nrParticles, dim), low = 0, high = boxLength)
-charges = np.random.choice((-1,1),nrParticles)
-# charges = np.ones(nrParticles)
-
-
-
+# %%
 #@jit(nopython=True)
 def pairwiseDistance(x, PBCs = False):
     """ Compute distances, optionally with PBCs
@@ -61,10 +62,9 @@ def kernel(x):
 #Idea: eventually give this as argument to `interaction' function
 
 
-def interaction(x,charges, PBCBool = True):
+def interaction(diff,dist,charges, PBCBool = True):
     """ Compute array of pairwise particle interactions for given array of particle coordinates and charges """
     
-    diff, dist = pairwiseDistance(x, PBCs = PBCBool)
     np.fill_diagonal(dist, 1) #set distance from particle to itself to non-zero value to avoid div by 0; arbitrary, since this term cancels out anyway
     chargeArray = charges * charges[:,np.newaxis] #create matrix b_i b_j
     distCorrected = (dist**2 + eps) #normalise to avoid computational problems at singularity
@@ -82,24 +82,41 @@ def projectParticles(x):
     return x
 
 
-
+# %%
 ### Simulation ###
 
 nrSteps = 500
-dt = 0.01
+dt = 0.001
 PBCs = True # Periodic Boundary Conditions
 eps = 0.00001
-sigma = 0.03 # influence of noise
+sigma = 0.01 # influence of noise
+sticky = True # whether or not collisions are sticky
+collTres = 0.01
 
 x = np.zeros((nrSteps, nrParticles, dim))
+chargesPerTime = np.zeros((nrSteps, nrParticles))
 x[0] = initialPositions
 simStartTime = timer.time() #to time simulation length
 
 for k in range(nrSteps-1):
-    determ = np.sum(interaction(x[k],charges, PBCBool = PBCs),axis = 1) #deterministic part
+    diff, dist = pairwiseDistance(x[k], PBCs = PBCs)
+    determ = np.sum(interaction(diff,dist,charges, PBCBool = PBCs),axis = 1) #deterministic part
     random = sigma * np.random.normal(size = (nrParticles,dim)) #'noise' part
     x[k+1] = x[k] + determ * dt +  random * np.sqrt(dt) 
     x = projectParticles(x) #places particles back into box
+    
+    if sticky: 
+        # Idea: dist matrix symmetrical and don't want diagonal, so only take entries above diagonal. 
+        #       need to compare to threshold, so np.triu does not work since all others are set to 0
+        #       so set everything on and below diagonal to arbitrary large value
+        dist += 10*np.tril(np.ones((nrParticles,nrParticles))) #arbitrary large number, so that effectiv
+        collidedPairs = np.where(dist < collTres) #format ([parts A], [parts B])
+        #TODO may want something nicer than this construction... 
+        
+        charges[collidedPairs[0]] = 0
+        chargesPerTime[collidedPairs[0]] = 0 #to keep track of colours in animated plot (not yet working)
+        x[k, collidedPairs[1]] = x[k, collidedPairs[0]]
+        
     
     if(k % (nrSteps/10) == 0):
         print(f"{k} / {nrSteps}")
@@ -110,12 +127,13 @@ print("Simulation duration was ", int(duration/3600), 'hours, ',
                                   int(duration%60), "seconds")  
 
 
-
+# %%
 ### Plot animation ###
 
-colorDict = {-1:'red', 1:'blue'}
-cols = np.vectorize(colorDict.get)(charges).tolist() #convert array of -1/1 to red/blue
-
+colorDict = {-1:'red', 0:'grey', 1:'blue'}
+cols = np.vectorize(colorDict.get)(charges).tolist() #convert array of -1/0/1 to red/grey/blue
+#TODO: this way, particles that collide at some point are gray from the beginning...
+#      can we set colours within update function?
 
 
 fig = plt.figure()
@@ -130,6 +148,7 @@ t_init = 0 # offset e.g. when disregarding warmup period
 N = nrParticles
 
 def update(i):
+    update.k += 1
     xs = x[i,:,0]
     ys = x[i,:,1]
     predata = np.array([xs,ys])
@@ -138,6 +157,7 @@ def update(i):
     time_text.set_text(time_template % (t_init+i*dt))
     return scat, time_text
 
+update.k = 0
 
-ani = animation.FuncAnimation(fig, update, frames=nrSteps, interval=dt*10)
+ani = animation.FuncAnimation(fig, update, frames=nrSteps, interval=dt*10, )
 plt.show()
