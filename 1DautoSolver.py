418e5d8fbc9310cd6ec0d2c38f758358c2777606
jmoraal <49272820+jmoraal@users.noreply.github.com>
2022-01-15 15:25:08 +0100

    Add (primitive) annihilation option

--- 1DautoSolver.py
+++ 1DautoSolver.py
@@ -10,20 +10,23 @@ import matplotlib.pyplot as plt
 import time as timer
 from scipy.integrate import solve_ivp
 
-### Main todos
-#TODO Find reasonable parameters
-#TODO Use NaNs in position array to be more memory and time efficient (note that dist computation is O(n²)!)
-#TODO make more class-based? May help a lot with readability etc
 
-
-### Simulation settings
+# %% Simulation settings
 simTime = 1000         # Total simulation time
 PBCs = False         # Whether to work with Periodic Boundary Conditions (i.e. see domain as torus)
-reg = 'cutoff'         # Regularisation technique; for now either 'eps' or 'cutoff' #TODO implement better regularisation, e.g. from Michiels20
-eps = 0.01        # To avoid singular force making computation instable. 
-boxLength = 6
+reg = 'eps'         # Regularisation technique; for now either 'eps' or 'cutoff' #TODO implement better regularisation, e.g. from Michiels20
+eps = 0.001        # To avoid singular force making computation instable. 
+collTres = 0.001
+boxLength = 1
+annihilation = True
+
 
 def setExample(N, boxLen = 1): 
+    '''
+    Sets initial configuration (location and charge) as global parameters  
+    for 3 minimal examples (N = 0,1,2) or any given number of dislocations (N > 2)
+
+    '''
     global boxLength, initialPositions, b, creations, domain, initialNrParticles
     
     boxLength = boxLen
@@ -48,9 +51,13 @@ def setExample(N, boxLen = 1):
     initialNrParticles = len(initialPositions)
     domain = (0,boxLength)
 
-setExample(100, boxLen = boxLength)
+setExample(1)#00, boxLen = boxLength)
 
-### Create grid, e.g. as possible sources: 
+# To plot: 
+bInitial = np.copy(b)
+annTimes = np.zeros(len(b))
+
+### Create grid, e.g. as possible sources for disloc. creation: 
 # nrSources = 11
 # sources = np.linspace(0,boxLength-1/(nrSources - 1), nrSources) # Remove last source, else have duplicate via periodic boundaries
 # # sources = np.array([0.21, 0.3, 0.45, 0.75, 0.8])
@@ -58,17 +65,17 @@ setExample(100, boxLen = boxLength)
 # nrBackgrSrc = 100 #Only to plot
 # backgrSrc = np.linspace(0,boxLength-1/(nrBackgrSrc - 1), nrBackgrSrc)
 
-# %%
-#@jit(nopython=True)
+# %% FUNCTIONS
+
 def pairwiseDistance(x1, PBCs = True, x2 = None):
-    """ Compute distances, optionally with PBCs
-    
-    Computes distances between all atoms, optionally for 
-    closest copies taking boundaries into account.
-    
-    INPUT: np array of coordinates (single or multiple dimensions)
+    """ Compute distances between all given coordinates, 
+    optionally with PBCs and optionally to a second given set of coordinates
+        
+    INPUT: array of coordinates (single or multiple dimensions)
+           (opt) boolean PBCs, whether to use periodic boundary conditions
+           (opt) second array of coordinates, to compute pairwise distance between both
     OUTPUT: array of difference vectors and one of distances 
-            between all coordinates, optionally w/ PBCs
+            between all coordinates
     """    
     
     if x2 is None: 
@@ -84,16 +91,21 @@ def pairwiseDistance(x1, PBCs = True, x2 = None):
 
 
 
-
 def f(t,x, regularisation = 'eps', PBCs = False):
-    """ Compute array of pairwise particle interactions for given array of particle coordinates and charges """
+    """ Compute discrete gradient flow for given array of coordinates 
+    according to Equation (9.1) from vMeurs15
+    
+    uses global parameters b (charges), eps, and 
+    updates global parameters b and annTimes 
+    
+    t is dummy argument used by ODE solver"""
     
     diff, dist = pairwiseDistance(x, PBCs = PBCs)
     np.fill_diagonal(dist, 1) # Set distance from particle to itself to (arbitrary) non-zero value to avoid div by 0; arbitrary, since this term cancels out anyway
     chargeArray = b * b[:,np.newaxis] # Create matrix b_i b_j
     
     if regularisation == 'eps': 
-        distCorrected = (dist**2 + eps**2) # Normalise to avoid computational problems at singularity. Square to normalise difference vector
+        distCorrected = (dist**2 + eps**2) 
     else: 
         distCorrected = dist**2
     
@@ -107,12 +119,23 @@ def f(t,x, regularisation = 'eps', PBCs = False):
     if regularisation == 'cutoff': 
         interactions = np.clip(interactions, -1/eps, 1/eps) # Sets all values outside [-c,c] to value of closest boundary
     
+    
     updates = np.nansum(interactions,axis = 1) 
     
+    if annihilation: 
+        bOld = np.copy(b) #to compare below, seeing whether annihilation happened this timestep
+        b[np.where((dist < collTres) * (chargeArray == -1))[0]] = 0 #so that only close and opposite-charged particles annihilate. * works as 'and'-operator for boolean (1/0) arrays
+        #TODO does not properly cover the case where e.g. three dislocations are close; then now, all annihilate (wrongly)
+        annTimes[bOld != b] = t #save for plotting
+        
     return updates
-    
+
+ 
 def projectParticles(x):
-    """Projects particles into box of given size."""
+    """Projects array of coordinates x into box of given size (global parameter boxLength).
+    
+    meant to use in case of Periodic boundary conditions (PBCs = True)
+    """
     
     x[np.isfinite(x)] %= boxLength # Also works in multi-D as long as box has same length in each dimension. Else, rewrite function to modulate in each dimension
     # Index by isfinite to not affect np.nan (still works otherwise, but gives warning)
@@ -127,15 +150,22 @@ simStartTime = timer.time() # To measure simulation computation time
 ### Simulation loop
 sol = solve_ivp(f, [0, simTime], initialPositions, method = 'BDF') #BDF is best for problems that may be stiff
 
+
 duration = timer.time() - simStartTime
 print("Simulation duration was ", int(duration/3600), 'hours, ', 
                                   int((duration%3600)/60), " minutes and ", 
                                   int(duration%60), "seconds")  
 
 
-# %% Visualisation: 
+# %% VISUALISATION: 
 
-def plotODESol(solution, charges, log = False): 
+def plotODESol(solution, charges, log = False, annihilationTimes = None): 
+    ''' Given a solution from solve_ivp, plots trajectories according to given charges. 
+    Optional: 
+     - plot on log-scale if log = True
+     - stop plotting if dislocations annihilated    
+    '''
+    
     plt.clf()
     t = sol.t
     x = np.transpose(sol.y)
@@ -151,15 +181,16 @@ def plotODESol(solution, charges, log = False):
     
     
     for i in range(nrParticles) : 
-        x_current = x[:,i]
+        t_current = t[t < annTimes[i]]
+        x_current = x[:len(t_current),i]
         x_temp = np.nan_to_num(x_current, nan = 10**5) # Work-around to avoid invalid values in np.where below
         pos = np.where(np.abs(np.diff(x_temp)) >= 0.5)[0]+1
         x_new = np.insert(x_current, pos, np.nan)
-        t_new = np.insert(t, pos, np.nan) # Insert NaNs in order not to draw jumps across domain caused by PBCs
+        t_new = np.insert(t_current, pos, np.nan) # Insert NaNs in order not to draw jumps across domain caused by PBCs
         # Note that x[pos] = np.nan would work as well, but that would delete data
         
-        plt.plot(x_new, t_new, c = colorDict.get(b[i]))
+        plt.plot(x_new, t_new, c = colorDict.get(charges[i]))
         
     plt.show()
 
-plotODESol(sol, b, log = True)
+plotODESol(sol, bInitial, log = True)
