69670151435c1c34d3c253db6d4dbd74984ddc7e
Moraal <jmc.moraal@gmail.com>
2022-04-26 14:17:37 +0200

    Add docstrings, improve parameter setting
    And fix _stupid_ mistake in PK force, finally fixing sign (and with that created dipole orientation)

--- 1DDislocationsCreation.py
+++ 1DDislocationsCreation.py
@@ -30,16 +30,16 @@ simTime = 0.5       # Total simulation time
 dt = 0.0005         # Timestep for discretisation (or initial, if adaptive timestep)
 adaptiveTime = True # Whether to use adaptive timestep in integrator
 PBCs = False        # Whether to work with Periodic Boundary Conditions (i.e. see domain as torus)
-reg = 'none'        # Regularisation technique; for now either 'eps', 'V1' (after vMeurs15), 'cutoff' or 'none' (only works when collTres > 0)
+reg = 'cutoff'        # Regularisation technique; for now either 'eps', 'V1' (after vMeurs15), 'cutoff' or 'none' (only works when collTres > 0)
 eps = 0.01          # Regularisation parameter
-cutoff = 50         # Regularisation parameter. Typically, force magnitude is 50 just before annihilation with eps=0.01
+cutoff = 100         # Regularisation parameter. Typically, force magnitude is 50 just before annihilation with eps=0.01
 randomness = False  # Whether to add random noise to dislocation positions (normal distr.)
 sigma = 0.01        # Standard dev. of noise (volatility)
 sticky = True       # Whether collisions are sticky, i.e. particles stay together once they collide (can probably be made standard)
-collTres = 0.005    # Collision threshold; if particles are closer than this, they are considered collided
+collTres = 0.005#e-7    # Collision threshold; if particles are closer than this, they are considered collided. Should be Should be very close to 0 if regularisation is used
 stress = 0          # External force (also called 'F'); only a constant in 1D case. Needed for creation in empty system
 autoCreation = True # Whether dipoles are introduced automatically according to creation rule (as opposed to explicit time and place specification)
-creaProc = 'zero'    # Creation procedure; either 'lin', 'zero' or 'dist' (for linear gamma, zero-gamma or distance creation respectively)
+creaProc = 'lin'    # Creation procedure; either 'lin', 'zero' or 'dist' (for linear gamma, zero-gamma or distance creation respectively)
 Fnuc = 0.5    # Threshold for magnitude of Peach-Koehler force 
 tnuc = 0.05     # Threshold for duration of PK force magnitude before creation
 Lnuc = 2*collTres   # Distance at which new dipole is introduced. Must be larger than collision threshold, else dipole annihilates instantly
@@ -49,53 +49,108 @@ domain = (0,1)      # Interval of space where initial dislocations and sources a
 
 
 def setExample(N): 
-    global initialPositions, b, creations, initialNrParticles
-        
-    if N == -1: ### Empty example; requires some adaptions in code below (e.g. turn off 'no dislocations left' break)
+    '''
+    Initialises positions and charges of given nr of dislocations
+
+    Parameters
+    ----------
+    N : int
+        number of dislocations to initialise. 0,1,2,5 gives fixed examples, 
+        other N give randomised settings with equally many +/- dislocations.
+
+    Returns
+    -------
+    None (sets global variables).
+
+    '''
+    global initialPositions, b, initialNrParticles
+    
+    if N == 0: ### Empty example; requires some adaptions in code below (e.g. turn off 'no dislocations left' break)
         initialPositions = np.array([np.nan, np.nan]) 
         b = np.array([1, -1])    
     
-    elif N == 0: ### Example 0: 
-        initialPositions = np.array([0.3, 0.75]) # If they are _exactly_ 0.5 apart, PBC distance starts acting up; difference vectors are then equal ipv opposite
-        b = np.array([1, -1])    
     
     elif N == 1: ### Example 1:
         initialPositions = np.array([0.49]) 
         b = np.array([1])
         
+        
+    elif N == 2: ### Example 2: 
+        initialPositions = np.array([0.3, 0.75]) # If they are _exactly_ 0.5 apart, PBC distance starts acting up; difference vectors are then equal ipv opposite
+        b = np.array([1, -1])    
     
-    elif N == 2: ### Example 2: manually appointed creation events
+    
+    elif N == 5: ### Example 3: 
         initialPositions = np.array([0.02, 0.2, 0.8, 0.85, 1]) 
         b = np.array([-1, -1, 1, 1, -1]) # Particle charges
     
-    
-    elif N == 3: ### Example 3: # 10 randomly distributed dislocations (5+,5-), with arbitrary (but 'manual') creation events
-        nrParticles = 10
-        initialPositions = np.random.uniform(size = nrParticles, low = 0, high = 1)
-        # 0/1 charges:
-        b = np.random.choice((-1,1),nrParticles)
-    
-    
-    else: 
+        
+    else: ### Given nr of particles, and option
         initialPositions = np.random.uniform(size = N) 
         b = np.ones(N)
-        neg = np.random.choice(range(N),N//2, replace=False)
+        neg = np.random.choice(range(N),N//2, replace=False) #pick floor(N/2) indices at random
         b[neg] = -1 # Set half of charges to -1, rest remains 1
+        # note: different from completely random charges, which would be the following: 
+        # b = np.random.choice((-1,1),nrParticles)
     
     
     # Dependent paramaters (deducable from the ones defined above): 
     initialNrParticles = len(initialPositions)
+    
+        
+
+def setSources(M, background = showBackgr): 
+    '''
+    Initialises positions and charges of given nr of sources
+
+    Parameters
+    ----------
+    M : int
+        number of sources to initialise (except -1). -1,1 gives fixed examples,
+        -1 serving as fixed reference case with 5 non-evenly distributed sources.
+        Other values give N sources evenly distributed on interval [0,1]
+    background : boolean, optional
+        indicates whether background sources should be initialised 
+        (only if PK force field is plotted). The default is showBackgr.
+
+    Returns
+    -------
+    None (sets global variables).
+
+    '''
+    global nrSources, sources, nrBackgrSrc, backgrSrc
+    # Initialise sources for creation: 
+    
+    if M == -1: 
+        sources = np.array([0.21, 0.3, 0.45, 0.75, 0.8])
+    
+    elif M == 1: 
+        sources = np.array([0.49])
+        
+    
+    else: # Evenly distribute given nr of sources
+        if PBCs: # Remove last source, else have duplicate via periodic boundaries
+            sources = np.linspace(0, 1-1/(M - 1), M) 
+        else: sources = np.linspace(0, 1, M)
+    
+    
+    nrSources = len(sources)
+    
+    if background: # Initialise additional sources for visualisation of PK-force: 
+        nrBackgrSrc = 100 
+        backgrSrc = np.linspace(0,1, nrBackgrSrc)
+
 
 setExample(1)
+if autoCreation: 
+    setSources(10)
 
-### Create grid, e.g. as possible sources: 
-nrSources = 11
-sources = np.linspace(0,1-1/(nrSources - 1), nrSources) # Remove last source, else have duplicate via periodic boundaries
+
+# Optional other sources: 
 # sources = np.array([0.49])
 # sources = np.array([0.21, 0.3, 0.45, 0.75, 0.8])
 # nrSources = len(sources)
-nrBackgrSrc = 100 #Only to plot
-backgrSrc = np.linspace(0,1-1/(nrBackgrSrc - 1), nrBackgrSrc)
+
 
 
 # %% FUNCTIONS
@@ -122,11 +177,31 @@ def pairwiseDistance(x1, PBCs = True, x2 = None):
     return diff, dist
 
 
-def interaction(diff,dist,b, PBCBool = True, regularisation = 'eps'):
-    """ Compute array of pairwise particle interactions for given array of particle coordinates and charges 
-    
-    Also returns array of pairwise charge products; not strictly necessary, but speeds up computation
-    """
+def interaction(diff,dist,b, PBCBool = False, regularisation = 'eps'):
+    '''
+    Compute array of pairwise particle interactions for given array of particle coordinates and charges 
+
+    Parameters
+    ----------
+    diff : 2D numpy array (nxn)
+        pairwise differences between all particle positions.
+    dist : 2D numpy array (nxn)
+        pairwise distances between all particle positions.
+    b : 1D numpy array (n)
+        charges of dislocations.
+    PBCBool : boolean, optional
+        whether to use periodic boundaries. The default is False.
+    regularisation : 'eps', 'V1', 'cutoff' or 'none', optional
+        interaction regularisation method. The default is 'eps'.
+
+    Returns
+    -------
+    interactions : 2D numpy array (nxn)
+        representing force pairs of particles exert on each other.
+    chargeArray : 2D numpy array (nxn)
+        array of pairwise charge products; not strictly necessary, but speeds up computation.
+
+    '''
     
     np.fill_diagonal(dist, 1) # Set distance from particle to itself to (aritrary) non-zero value to avoid div by 0; arbitrary, since this term cancels out anyway
     chargeArray = b * b[:,np.newaxis] # Create matrix b_i b_j
@@ -165,7 +240,7 @@ def PeachKoehler(sources, x, b, stress, regularisation = 'eps'):
     (Sources are typically fixed equidistant grid points) """
     
     x = np.nan_to_num(x) #sets NaNs to 0
-    dist, diff = pairwiseDistance(x, x2 = sources) 
+    diff, dist = pairwiseDistance(x, x2 = sources) 
     
     if regularisation == 'eps': 
         distCorrected = (dist**2 + eps**2) # Normalise to avoid computational problems at singularity. Square to normalise difference vector
@@ -217,10 +292,11 @@ while t < simTime:
         tresHist[np.abs(PK) < Fnuc] = 0 # Set all others to 0
         creations = np.where(tresHist >= tnuc)[0] # Identify which sources reached time threshold (automatically reached force threshold too). [0] to 'unpack' array from tuple
         nrNewCreations = len(creations)
-        PKNewCreations = np.abs(PK[creations])
+        PKNewCreations = PK[creations]
         nrNewDislocs = nrNewCreations * 2
         if nrNewDislocs > 0:
             tresHist[creations] = 0 # Reset counters of new creations
+            
             
             # Set counters for exception time, to keep track of force exception:
             if creaProc == 'lin': #TODO need zeros of Rcrit from ODEPhaseplot here! 
@@ -245,10 +321,10 @@ while t < simTime:
                 locs[1::2] = sources[creations] + 0.5*collTres 
             
             charges = np.zeros(nrNewDislocs)
-            
+            print(np.sign(PKNewCreations))
             for i in range(nrNewCreations):
-                sign = np.sign(PK[creations[i]]) # Index from 'creations' because PK is computed for all sources, not only new creations
-                charges[2*i] = sign #TODO check sign! Might be other way around
+                sign = np.sign(PKNewCreations[i]) # Index from 'creations' because PK is computed for all sources, not only new creations
+                charges[2*i] = sign 
                 charges[2*i+1] = -sign 
             
             
@@ -317,7 +393,7 @@ while t < simTime:
         #       so set everything on and below diagonal to arbitrary large value
         newDist += 1000*np.tril(np.ones((len(x_new),len(x_new)))) # Arbitrary large number, so that effectively all entries on and below diagonal are disregarded #TODO should this be x or x_new?
         collidedPairs = np.where((newDist < collTres) & (chargeArray == -1)) # Format: ([parts A], [parts B]). Makes sure particles only annihilate if they have opposite charges
-        #TODO may want something nicer than this construction... 
+        #TODO need something better than this construction! Does not facilitate three-particle collisions
         
         
         b[collidedPairs[0]] = 0 # If non-integer charges: take (b[collidedPairs[0]] + b[collidedPairs[1]])/2
@@ -365,15 +441,16 @@ def plot1D(bInitial, trajectories, t, PK = None, log = False):
     
     
     for i in range(nrTrajectories):
-        #insert NaNs at 'discontinuities':
         x_current = trajectories[:,i]
-        x_temp = np.nan_to_num(x_current, nan = 10**5) # Work-around to avoid invalid values in np.where below
-        pos = np.where(np.abs(np.diff(x_temp)) >= 0.5)[0]+1
-        x_new = np.insert(x_current, pos, np.nan)
-        y_new = np.insert(y, pos, np.nan) # Insert NaNs in order not to draw jumps across domain caused by PBCs
-        # Note that x[pos] = np.nan would work as well, but that would delete data
+        y_current = y
+        if PBCs: #insert NaNs at 'discontinuities':
+            x_temp = np.nan_to_num(x_current, nan = 10**5) # Work-around to avoid invalid values in np.where below
+            pos = np.where(np.abs(np.diff(x_temp)) >= 0.5)[0]+1 # find jumps across domain caused by PBCs
+            x_current = np.insert(x_current, pos, np.nan) # Insert NaNs in order not to draw jumps
+            y_new = np.insert(y, pos, np.nan) 
+            # Note that x[pos] = np.nan would work as well, but that would delete data
         
-        plt.plot(x_new, y_new, c = colorDict.get(bInitial[i]))
+        plt.plot(x_current, y_current, c = colorDict.get(bInitial[i]))
         # Set colour of created dislocations according to charge they eventually get (not 0, which they begin with)
         
     if not PK is None:
